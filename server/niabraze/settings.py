22cf1d29745cd51892c4e48b2b8ba416eefa848f
DJFancyAl <abluemleiv@gmail.com>
2023-10-09 16:04:32 -0400

    Updated

--- server/niabraze/settings.py
+++ server/niabraze/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['niabraze-quote-calculator.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['niabraze-quote-calculator.onrender.com']
 
 # Application definition
 
